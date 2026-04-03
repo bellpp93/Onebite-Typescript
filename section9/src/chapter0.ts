a8a7fe6cd46f4c14ca621a62ddd43decd1140b60
JongIn Yoon <bellpp93@gmail.com>
2026-04-03 22:26:14 +0900

    Conditional Types_20260403

---
+++ section9/src/chapter0.ts
@@ -0,0 +1,26 @@
+/**
+ * 조건부 타입
+ */
+
+type A = number extends string ? string : number;
+
+type ObjA = {
+  a: number;
+};
+
+type ObjB = {
+  a: number;
+  b: number;
+};
+
+type B = ObjB extends ObjA ? number : string;
+
+/**
+ * 제네릭과 조건부 타입
+ */
+
+type StringNumberSwitch<T> = T extends number ? string : number;
+
+let varA: StringNumberSwitch<number>;
+
+let varB: StringNumberSwitch<string>;
