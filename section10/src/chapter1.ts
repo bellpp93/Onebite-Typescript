053fe47fe6d91a9633c035d4c2fb9f96428cb88e
JongIn Yoon <bellpp93@gmail.com>
2026-04-10 00:22:20 +0900

    Partial<T>_Required<T>_Readonly<T>_20260410

--- section10/src/chapter1.ts
+++ section10/src/chapter1.ts
@@ -4,3 +4,54 @@
  * -> 특정 객체 타입의 모든 프로퍼티를 선택적 프로퍼티로 바꿔주는 타입
  */
 
+interface Post {
+  title: string;
+  tags: string[];
+  content: string;
+  thumbnailURL?: string;
+}
+
+type Partial<T> = {
+  [key in keyof T]?: T[key];
+};
+
+const draft: Partial<Post> = {
+  title: "제목 나중에",
+  content: "초안...",
+};
+
+/**
+ * Required<T>
+ * -> 필수의, 필수적인
+ * -> 특정 객체 타입의 모든 프로퍼티를 필수 프로퍼티로 바꿔주는 타입
+ */
+
+type Required<T> = {
+  [key in keyof T]-?: T[key];
+};
+
+const withThumbnailPost: Post = {
+  title: "한입 타입스크립트 후기",
+  tags: ["ts"],
+  content: "",
+  thumbnailURL: "https://...",
+};
+
+/**
+ * Readonly<T>
+ * -> 읽기전용 수정불가
+ * -> 특정 객체 타입에서 모든 프로퍼티를 읽기 전용 프로퍼티로 만들어주는 타입
+ */
+
+type Readonly<T> = {
+  readonly [key in keyof T]: T[key];
+};
+
+const readonlyPost: Readonly<Post> = {
+  title: "보호된 게시글 입니다.",
+  tags: [],
+  content: "",
+};
+
+// readonly이기 때문에 수정불가!
+// readonlyPost.content = "";
