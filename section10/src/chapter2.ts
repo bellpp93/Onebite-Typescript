15e1d98331815c25ec99bd610fad99303a1d6677
JongIn Yoon <bellpp93@gmail.com>
2026-04-18 00:36:40 +0900

    Omit, Record Type_20260418

--- section10/src/chapter2.ts
+++ section10/src/chapter2.ts
@@ -21,3 +21,49 @@ const legacyPost: Pick<Post, "title" | "content"> = {
   title: "옛날 글",
   content: "옛날 컨텐츠",
 };
+
+/**
+ * Omit<T, K>
+ * -> 생략하다, 빼다
+ * -> 객체 타입으로부터 특정 프로퍼티를 제거하는 타입
+ */
+
+type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
+// T = Post, K = 'title'
+// Pick<Post, Exclude<keyof Post, 'title'>>
+// Pick<Post, Exclude<'title' | 'content' | 'tags' | 'thumbnailURL', 'title'>>
+// Pick<Post, 'content' | 'tags' | 'thumbnailURL'>
+
+const noTitlePost: Omit<Post, "title"> = {
+  content: "",
+  tags: [],
+  thumbnailURL: "",
+};
+
+/**
+ * Record<K, V>
+ */
+
+// type ThumbnailLegacy = {
+//   large: {
+//     url: string;
+//   };
+//   medium: {
+//     url: string;
+//   };
+//   small: {
+//     url: string;
+//   };
+//   watch: {
+//     url: string;
+//   };
+// };
+
+type Record<K extends keyof any, V> = {
+  [key in K]: V;
+};
+
+type Thumbnail = Record<
+  "large" | "medium" | "small" | "watch",
+  { url: string; size: number }
+>;
