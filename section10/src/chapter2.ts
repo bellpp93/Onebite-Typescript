4e3dcd3de56230068c64d089e529cfa8e157ed50
JongIn Yoon <bellpp93@gmail.com>
2026-04-17 23:58:48 +0900

    Pick Type_20260417

---
+++ section10/src/chapter2.ts
@@ -0,0 +1,23 @@
+/**
+ * Pick<T, K>
+ * -> 뽑다, 고르다
+ * -> 객체 타입으로부터 특정 프로퍼티만 딱 골라내는 그런 타입
+ */
+
+interface Post {
+  title: string;
+  tags: string[];
+  content: string;
+  thumbnailURL?: string;
+}
+
+type Pick<T, K extends keyof T> = {
+  // K extends 'title' | 'tags' | 'content' | 'thumbnailURL'
+  // 'title' | 'content' extends 'title' | 'tags' | 'content' | 'thumbnailURL'
+  [key in K]: T[key];
+};
+
+const legacyPost: Pick<Post, "title" | "content"> = {
+  title: "옛날 글",
+  content: "옛날 컨텐츠",
+};
